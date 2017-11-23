35ad302dbb0f3f3054b40545f7862aa6109dcf4d
Saad Mecci <saadmecci@yahoo.com>
2017-11-22 18:06:50 -0600

    ormFunctions

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,94 @@
 //import MySQL connection
 var connection = require("./connection.js");
+
+
+// elper function for SQL syntax
+// helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(number) {
+	var array = [];
+
+	for (var i = 0; i < number; i++) {
+		array.push("?");
+	}
+
+	return array.toString();
+}
+
+//helper function to convert object key/value pairs to SQL syntax
+function objectToSql(object) {
+	var array = [];
+
+	//loop through the keys and push the key/value as a string int arr
+	for (var key in object) {
+		var value = object[key];
+		// check to skip hidden properties
+		if (Object.hasOwnProperty.call(object, key)) {
+			//if string with spaces, add quotations (Big Burger => "Big Burger")
+			if (typeof value === "string" && value.indexOf(" ") >= 0) {
+				value = "'" + value + "'";
+      		}
+      		//{eaten: true} => ["eaten=true"]
+			array.push(key + "=" + value);
+    	}
+	}
+	//translate array of strings to a single comma-separated string
+	return array.toString();
+}
+
+//object for the SQL functions that will be called later
+var orm = {
+	selectAll: function (tableInput, callback) {
+		//set variable to use in MySQL connection.query function
+		var queryString = "SELECT * FROM " + tableInput + ";";
+		//selects everything in the table to be viewed
+		connection.query(queryString, function (error, results) {
+			//if function errors, stop script
+			if (error) {
+				throw error;
+			}
+			//return the results from the MySQL query into the selectAll method
+			callback(results);
+		});
+	},
+	insertOne: function (table, columns, values, callback) {
+		//set variable to use in MySQL connection.query function
+		var queryString = "INSERT INTO " + table;
+		//updates variable with the clumn name and corresponding value to be entered
+		queryString += " (";
+	    queryString += columns.toString();
+	    queryString += ") ";
+	    queryString += "VALUES (";
+	    queryString += printQuestionMarks(values.length);
+	    queryString += ") ";
+	    //inserts a row into the table based on column name and value
+	    connection.query(queryString, function (error, results) {
+	    	//if function errors, stop script
+	    	if (error) {
+	    		throw error;
+	    	}
+	    	//return the results from MySQL query into the insertOne method
+	    	callback(results);
+	    });
+	},
+	updateOne: function (table, objectColumnValues, condition, callback) {
+		//set variable to use in MySQL connection.query function
+		var queryString = "UPDATE " + table;
+		//updates variable with the clumn name and corresponding value to be entered
+		queryString += " SET ";
+	    queryString += objectToSql(objColumnValues);
+	    queryString += " WHERE ";
+	    queryString += condition;
+	    //updates an existing entry in the SQL table
+	    connection.query(queryString, function (error, results) {
+	    	//if function errors, stop script
+	    	if (error) {
+	    		throw error;
+	    	}
+	    	//return the results from MySQL query into the updateOne method
+	    	callback(results);
+	    });
+	}
+};
+//exports the orm object for the model (burger.js)
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+
+var port = 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
